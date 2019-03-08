b8d1d1798d23f43edaa3b31b99683a103c646c31
caspersu <48322723+caspersu@users.noreply.github.com>
2019-03-08 13:58:25 +0800

    Tool Management Web

---
+++ WebApplication/Tool_Management.Service/Interfaces/ITM_Model.cs
@@ -0,0 +1,16 @@
+﻿using Kendo.Mvc.UI;
+using Tool_Management.Service.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool_Management.Service.Interfaces
+{
+    public interface ITM_Model
+    {
+        DataSourceResult GridModelData(DataSourceRequest request);
+    }
+
+}
---
+++ WebApplication/Tool_Management.Service/ViewModels/ModelViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool_Management.Service.ViewModels
+{
+    public class ModelViewModel
+    {
+        [DisplayName("機種編號")]
+        [Required]
+        public string Model_ID {get; set;}
+        [DisplayName("機種名稱")]
+        [Required]
+        public string Model_Name { get; set; }
+        [DisplayName("建立人員")]       
+        public string Model_Create_ID { get; set; }
+        [DisplayName("建立時間")]
+        public DateTime Model_Create_DT { get; set; }
+        [DisplayName("修改人員")]
+        public string Model_Modify_ID { get; set; }
+        [DisplayName("修改時間")]
+        public DateTime Model_Modify_DT { get; set; }
+    }
+}
